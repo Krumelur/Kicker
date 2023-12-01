e95bfca27773351f55d3d45903590f4309d56069
René Ruppert <rene.ruppert@gmail.com>
2023-12-01 10:08:26 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd src
+# IMPORTANT: For the game to autostart, run 'sudo chmod +x run.sh' in the terminal.
+#            Then add the line '@lxterminal /home/pi/Documents/Develop/Kicker/run.sh' to
+#            the file '/etc/xdg/lxsession/LXDE-pi/autostart'.
+cd /home/pi/Documents/Develop/Kicker/src
 python main.py
