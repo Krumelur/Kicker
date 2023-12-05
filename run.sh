ca1d9025c2aa17a9b8f18275c8eb47f6cfff37c7
René Ruppert <rene.ruppert@gmail.com>
2023-12-05 21:03:12 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@
 #            Then add the line '@lxterminal /home/pi/Documents/Develop/Kicker/run.sh' to
 #            the file '/etc/xdg/lxsession/LXDE-pi/autostart'.
 cd /home/pi/Documents/Develop/Kicker/src
+python -m venv defaultenv
+source defaultenv/bin/activate
+pip install -r ../requirements.txt
 python main.py
